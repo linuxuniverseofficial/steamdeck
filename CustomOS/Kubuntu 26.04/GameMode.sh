5c00011b3f18b94856d5500dbc53e9c340eba37d
José Humberto Ferreira Júnior <josehumberto.fj@gmail.com>
2026-06-30 00:30:49 -0300

    Delete CustomOS/Kubuntu 26.04/GameMode.sh

--- CustomOS/Kubuntu 26.04/GameMode.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# 1. Altera o arquivo para Modo Jogo
-sed -i 's/Session=.*/Session=steam-gamescope/' /etc/sddm.conf.d/20-kubuntu.conf
-sed -i 's/User=.*/User=ztge/' /etc/sddm.conf.d/20-kubuntu.conf
-sed -i 's/Relogin=.*/Relogin=true/' /etc/sddm.conf.d/20-kubuntu.conf
-sync
-
-# 2. Reinicia o SDDM de forma limpa
-systemctl restart sddm
